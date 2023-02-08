554f0ecfec1799da513548402feb9670f685592c
zhangqiang <@892750407@qq.com>
2023-02-08 09:41:16 +0800

    计算器代码

--- app/src/main/java/com/example/studyplan/CalcuAdapter.java
+++ app/src/main/java/com/example/studyplan/CalcuAdapter.java
@@ -1,8 +1,12 @@
 package com.example.studyplan;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.viewholder.BaseViewHolder;
@@ -12,6 +16,7 @@ import java.util.List;
 
 public class CalcuAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
     private Context mContext;
+
     public CalcuAdapter(@Nullable List<String> data, Context context) {
         super(R.layout.calculategrid, data);
         mContext = context;
--- app/src/main/java/com/example/studyplan/CalculateActivity.java
+++ app/src/main/java/com/example/studyplan/CalculateActivity.java
@@ -1,12 +1,17 @@
 package com.example.studyplan;
 
-
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -17,39 +22,116 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.listener.OnItemClickListener;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import Model.News;
-
-public class CalculateActivity extends AppCompatActivity {
+public class CalculateActivity extends AppCompatActivity{
     private CalcuAdapter mAdapter;
     private RecyclerView rv;
+    public List<String> list = new ArrayList<>();
+    private String lastNum;
+    private String nextNum;
+    private boolean prepare;
+    private int type;//1加 2减 3乘 4除 0无状态
+    private StringBuilder sb;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculatesimulator);
-        List<String> list = new ArrayList<>();
         String[] strs = {"1","2","3","4","5","6","7","8","9","0","c","+",
                 "-","X","÷","="};
         for (String obj :strs){
             list.add(obj);
         }
-        /*
-        * 九宫格布局，列表，都用recyclerview做，然后在设置布局管理器的时候区分layoutmanager就可以，这个三方还是很好用的
-        * */
-        GridLayoutManager layoutManager = new GridLayoutManager(this,4);
-        rv = findViewById(R.id.calcuGrid);
-        rv.setLayoutManager(layoutManager);
-        mAdapter = new CalcuAdapter(list,this);
-        rv.setAdapter(mAdapter);
-        mAdapter.setOnItemClickListener(new OnItemClickListener() {
+        GridView gridView = findViewById(R.id.channel);
+        TextView tv = findViewById(R.id.resultLabel);
+        MyAdapter adapter = new MyAdapter(this,list);
+        gridView.setAdapter(adapter);
+        sb = new StringBuilder();
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(@NonNull BaseQuickAdapter<?, ?> adapter,
-                                    @NonNull View view, int position) {
-                Log.e("","点击了符号："+ list.get(position) + "");
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                calculateResult(position,tv);
             }
         });
+    }
+
+    void calculateResult(int position,TextView tv){
+        if (position<10){
+            sb.append(list.get(position));
+            tv.setText(sb.toString());
+            if (!prepare ){
+                lastNum = sb.toString();
+            }else {
+                nextNum = sb.toString();
+            }
+        }else if (position > 10 && position < 15){
+            prepare = true;
+            type = position-10;
+            sb = new StringBuilder();
+        }else if (position == 15){
+            int result = 0;
+            prepare = false;
+            sb = new StringBuilder();
+            if (type == 1){
+                result =
+                        Integer.parseInt(lastNum)+Integer.parseInt(nextNum);
+            }else if (type == 2){
+                result =
+                        Integer.parseInt(lastNum)-Integer.parseInt(nextNum);
+            }else if (type == 3){
+                result =
+                        Integer.parseInt(lastNum)*Integer.parseInt(nextNum);
+            }else if (type == 4){
+                result =
+                        Integer.parseInt(lastNum)/Integer.parseInt(nextNum);
+            }
+            tv.setText(String.valueOf(result));
+        }else if (position == 10){
+            tv.setText("0");
+            prepare = false;
+            sb = new StringBuilder();
+            lastNum = "";
+            nextNum = "";
+        }
+    }
+}
+
+class MyAdapter extends BaseAdapter {
+    private Context mcontext;
+    private LayoutInflater layoutInflater;
+    private List<String > list;
+
+    MyAdapter(Context context,List<String> list) {
+        this.mcontext = context;
+        layoutInflater = LayoutInflater.from(context);
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = layoutInflater.inflate(R.layout.calculategrid,null);
+        TextView tv = v.findViewById(R.id.grid_item);
+        tv.setText(list.get(position));
+        return v;
     }
 }
